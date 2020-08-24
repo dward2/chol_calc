931e53ad03f0865dfc6a833dec59e2a28741e048
David Ward <david.a.ward@duke.edu>
2020-08-24 10:01:35 -0400

    Gets HDL input from the user in terminal

--- calculator.py
+++ calculator.py
@@ -1,11 +1,23 @@
+def get_HDL_input():
+    HDL_input = input("Enter the HDL level: ")
+    HDL_value = int(HDL_input)
+    return HDL_value
+
+def HDL():
+    
+
+
 def interface():
     print("My Program")
     while True:
         print("Options:")
+        print("1-HDL Check")
         print("9-Quit")
         choice = input("Enter your choice: ")
         if choice == '9':
             return
+        elif choice == '1'"
+            HDL()
 
 
 interface()
